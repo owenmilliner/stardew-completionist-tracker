9bd1e7b856ff371c9f96f26d99a35cb4fc6e7655
owenmilliner <owenmilliner1@gmail.com>
2022-03-31 11:37:54 +0100

    👮‍Add eslint rule inforcement.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
+import React from 'react';
+
 const Header = () => {
-    return (
-        <div className='header'>
-            <img id="header__logo" src={require('../images/logo.png')} alt="Stardew Valley Logo"/>
-            <img id="header__subtitle" src={require('../images/subtitle.png')} alt="Completion Tracker (Subtitle)"/>
-        </div>
-    )
+  return (
+    <div className='header'>
+      <img id="header__logo" src={require('../images/logo.png')} alt="Stardew Valley Logo"/>
+      <img id="header__subtitle" src={require('../images/subtitle.png')} alt="Completion Tracker (Subtitle)"/>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,31 +1,33 @@
-const Instructions = () => {
-    return (
-        <div className='instructions'>
-            <div className='instructions__overview instructions__panel'>
-                <h2>How it works.</h2>
-                <p>This is a placeholder description. This will include data on:</p>
-                <ul>
-                    <li className='instructions__overview__list'>
-                        Uploading save file / manual input
-                    </li>
-                    <li>
-                        Simple display of incomplete criteria (sortable by season, etc)
-                    </li>
-                </ul>
-            </div>
-            <div className='instructions__start instructions__panel'>
-                <h2>Uploading a save file.</h2>
-                <p>Placeholder for instructions.</p>
+import React from 'react';
 
-                <h2>Manual input.</h2>
-                <p>Placeholder for instructions.</p>
-            </div>
-            <div className='instructions__resources instructions__panel'>
-                <h2>Additional useful resources.</h2>
-                <p>Placeholder for description.</p>
-            </div>
-        </div>
-    )
+const Instructions = () => {
+  return (
+    <div className='instructions'>
+      <div className='instructions__overview instructions__panel'>
+        <h2>How it works.</h2>
+        <p>This is a placeholder description. This will include data on:</p>
+        <ul>
+          <li className='instructions__overview__list'>
+                        Uploading save file / manual input
+          </li>
+          <li>
+                        Simple display of incomplete criteria (sortable by season, etc)
+          </li>
+        </ul>
+      </div>
+      <div className='instructions__start instructions__panel'>
+        <h2>Uploading a save file.</h2>
+        <p>Placeholder for instructions.</p>
+
+        <h2>Manual input.</h2>
+        <p>Placeholder for instructions.</p>
+      </div>
+      <div className='instructions__resources instructions__panel'>
+        <h2>Additional useful resources.</h2>
+        <p>Placeholder for description.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Instructions;
